afe6acec3398fd6c3ae7a0a3108dcbc06ae64c17
Gabe Grant <gabe.grant@proton.me>
2023-02-24 12:43:52 -0500

    VS changes

--- ILoveYouALatte/obj/Release/net6.0/ILoveYouALatte.AssemblyInfo.cs
+++ ILoveYouALatte/obj/Release/net6.0/ILoveYouALatte.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8cd10b06-4184-4b4e-904e-c7318d37b506")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ILoveYouALatte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
